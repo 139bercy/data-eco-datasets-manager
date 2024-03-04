7d6a5a5a863b53dae377bc3a213a15d6d137aaa3
tongyeouki <tongyeouki@gmail.com>
2024-03-04 08:13:22 +0100

    chore: fix comments in tests

--- tests/test_dataset.py
+++ tests/test_dataset.py
@@ -5,32 +5,32 @@ from adapters.primaries import DatasetCsvRepository, DatasetApiRepository
 
 
 def test_should_retrieve_a_list_of_datasets_from_csv():
-    # Act
+    # Arrange
     repository = DatasetCsvRepository("datasets.csv")
     repository.path = "tests/fixtures/"
-    # Arrange
+    # Act
     datasets = repository.get_all()
     # Assert
     assert len(datasets) == 3
 
 
 def test_should_retrieve_one_dataset_from_csv():
-    # Act
+    # Arrange
     repository = DatasetCsvRepository("datasets.csv")
     repository.path = "tests/fixtures/"
     dataset_id = "test-dataset-1"
-    # Arrange
+    # Act
     dataset = repository.get_one(dataset_id=dataset_id)
     # Assert
     assert dataset["dataset_id"] == dataset_id
 
 
 def test_should_retrieve_a_list_of_datasets_from_api():
-    # Act
+    # Arrange
     repository = DatasetApiRepository()
     with open("tests/fixtures/datasets.json", "r") as file:
         content = file.read()
-    # Arrange
+    # Act
     with requests_mock.Mocker() as m:
         m.get(f"{DOMAIN_NAME}/api/automation/v1.0/datasets/", text=content)
         datasets = repository.get_all()
@@ -39,11 +39,11 @@ def test_should_retrieve_a_list_of_datasets_from_api():
 
 
 def test_should_retrieve_one_dataset_from_api():
-    # Act
+    # Arrange
     repository = DatasetApiRepository()
     with open("tests/fixtures/dataset.json", "r") as file:
         content = file.read()
-    # Arrange
+    # Act
     with requests_mock.Mocker() as m:
         m.get(f"{DOMAIN_NAME}/api/explore/v2.1/catalog/datasets/", text=content)
         datasets = repository.get_one("test-dataset-1")
