f4755c69d771c2cc470f9b027dc7c20e6c4bf68e
tongyeouki <tongyeouki@gmail.com>
2024-11-12 18:50:34 +0100

    tests: update tests

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,23 +1,23 @@
 import os
 
 from users.repositories import InMemoryUsersRepository, TinyDbUserRepository
-from users.usecases import create_user
+from users.usecases import import_user
 
 
-def test_create_user(users_fixture):
+def test_import_user(users_fixture):
     # Arrange
     repository = InMemoryUsersRepository([])
     # Act
-    user = create_user(repository=repository, user=users_fixture)
+    user = import_user(repository=repository, user=users_fixture)
     # Assert
     assert len(repository.db) == 1
     assert user.buid == "c0c10d06"
-
+    
 
 def test_update_user(users_fixture):
     # Arrange
     repository = InMemoryUsersRepository([])
-    create_user(repository=repository, user=users_fixture)
+    import_user(repository=repository, user=users_fixture)
     updated = {**users_fixture, "last_seen_at": "2024-01-02T12:00:00Z"}
     # Act
     repository.update(username="john.doe", values=updated)
@@ -31,7 +31,7 @@ def test_update_user(users_fixture):
 def test_upsert_user(users_fixture):
     # Arrange
     repository = InMemoryUsersRepository([])
-    user = create_user(repository=repository, user=users_fixture)
+    user = import_user(repository=repository, user=users_fixture)
     user.update(last_seen_at="2024-01-02T12:00:00Z")
     # Act
     repository.upsert(user=user)
@@ -41,11 +41,11 @@ def test_upsert_user(users_fixture):
     assert user.last_seen_at == "2024-01-02T12:00:00Z"
 
 
-def test_create_user_on_tinydb(users_fixture):
+def test_import_user_on_tinydb(users_fixture):
     # Arrange
     repository = TinyDbUserRepository("db-test.json")
     # Act
-    user = create_user(repository=repository, user=users_fixture)
+    user = import_user(repository=repository, user=users_fixture)
     # Assert
     assert len(repository.all()) == 1
     assert user.buid == "c0c10d06"
@@ -55,7 +55,7 @@ def test_create_user_on_tinydb(users_fixture):
 def test_update_user_on_tinydb(users_fixture):
     # Arrange
     repository = TinyDbUserRepository("db-test.json")
-    create_user(repository=repository, user=users_fixture)
+    import_user(repository=repository, user=users_fixture)
     updated = {"last_seen_at": "2024-01-02T12:00:00Z"}
     # Act
     repository.update(username="john.doe", values=updated)
@@ -69,7 +69,7 @@ def test_update_user_on_tinydb(users_fixture):
 def test_upsert_user_on_tinydb(users_fixture):
     # Arrange
     repository = TinyDbUserRepository("db-test.json")
-    user = create_user(repository=repository, user=users_fixture)
+    user = import_user(repository=repository, user=users_fixture)
     user.update(last_seen_at="2024-01-02T12:00:00Z")
     # Act
     repository.upsert(user=user)
